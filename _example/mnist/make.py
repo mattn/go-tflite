0c2d8817124e6bee1eabfcfbd6ae72503fd09414
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2019-03-16 01:15:29 +0900

    Add notebook

--- _example/mnist/make.py
+++ _example/mnist/make.py
@@ -6,8 +6,6 @@ from tensorflow.contrib.keras.api.keras.utils import to_categorical
 import tensorflow.contrib.lite as lite
 
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
-#x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)
-#x_test = x_test.reshape(x_test.shape[0], 28, 28, 1)
 x_train  = x_train.reshape(60000, 784)
 x_test   = x_test.reshape(10000, 784)
 x_train  = x_train.astype('float32')

db05f9484921f9228baef7ea705aa5b205dd08b8
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2019-03-22 13:31:53 +0900

    Support quant scale/zeropoint

--- _example/webcam/main.go
+++ _example/webcam/main.go
@@ -11,6 +11,7 @@ import (
 	_ "image/png"
 	"log"
 	"os"
+	"os/signal"
 	"sort"
 	"sync"
 	"time"
@@ -33,12 +34,21 @@ var (
 	labelPath = flag.String("label", "labels.txt", "path to label file")
 )
 
-type quantResult struct {
+type quantUInt8Result struct {
 	output []byte
 	img    image.Image
 }
 
-func (r *quantResult) Image() image.Image {
+func (r *quantUInt8Result) Image() image.Image {
+	return r.img
+}
+
+type quantFloat32Result struct {
+	output []float32
+	img    image.Image
+}
+
+func (r *quantFloat32Result) Image() image.Image {
 	return r.img
 }
 
@@ -115,10 +125,23 @@ func capture(wg *sync.WaitGroup, frameChan chan image.Image, ctx context.Context
 		}
 		bounds := img.Bounds()
 		height := 500 * bounds.Dy() / bounds.Dx()
-		win.SetBounds(pixel.R(0, 0, 500, float64(height)))
-
+		if height != int(win.Bounds().H()) {
+			win.SetBounds(pixel.R(0, 0, 500, float64(height)))
+		}
 		frameChan <- resize.Resize(500, uint(height), img, resize.NearestNeighbor)
 	}
+}
+
+func maxFloat32(f []float32) (int, float32) {
+	mi := 0
+	mf := float32(0)
+	for i := 1; i < len(f); i++ {
+		if mf < f[i] {
+			mi = i
+			mf = f[i]
+		}
+	}
+	return mi, mf
 }
 
 func run() {
@@ -129,7 +152,7 @@ func run() {
 
 	// Setup Pixel window
 	cfg := pixelgl.WindowConfig{
-		Title:  "Thinger",
+		Title:  "webcam",
 		Bounds: pixel.R(0, 0, 500, 500),
 		VSync:  true,
 	}
@@ -175,14 +198,21 @@ func run() {
 	wanted_channels := input.Dim(3)
 
 	// Start up the background capture
-	frameChan := make(chan image.Image, 2)
-	resultChan := make(chan result, 2)
+	frameChan := make(chan image.Image, 1)
+	resultChan := make(chan result, 1)
 	go capture(&wg, frameChan, ctx, cam, win)
 	go detect(&wg, resultChan, frameChan, ctx, interpreter, wanted_width, wanted_height, wanted_channels)
 
 	// Setup Pixel requirements for drawing boxes and labels
 	atlas := text.NewAtlas(basicfont.Face7x13, text.ASCII)
 	imd := imdraw.New(nil)
+
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, os.Interrupt)
+	go func() {
+		<-sc
+		win.Destroy()
+	}()
 
 	// Some local vars to calculate frame rate
 	var (
@@ -243,7 +273,37 @@ func run() {
 				txt.Draw(win, pixel.IM.Scaled(txt.Orig, 1))
 			}
 			imd.Draw(win)
-		case *quantResult:
+		case *quantFloat32Result:
+			b := t.output
+			classes := make([]quantClass, 0, len(b))
+			var i int
+			for i = 0; i < len(b); i++ {
+				score := float64(b[i])
+				if score < 0.2 {
+					continue
+				}
+				classes = append(classes, quantClass{score: score, index: i})
+			}
+			sort.Slice(classes, func(i, j int) bool {
+				return classes[i].score > classes[j].score
+			})
+
+			if len(classes) > 5 {
+				classes = classes[:5]
+			}
+			h := win.Bounds().H()
+			for i, class := range classes {
+				label := "unknown"
+				if class.index < len(labels) {
+					label = labels[class.index]
+				}
+				s := fmt.Sprintf("%d %.5f %s\n", i, class.score, label)
+				txt := text.New(pixel.V(10.0, h-10-float64(10*i)), atlas)
+				txt.Color = color.White
+				txt.WriteString(s)
+				txt.Draw(win, pixel.IM.Scaled(txt.Orig, 1))
+			}
+		case *quantUInt8Result:
 			b := t.output
 			classes := make([]quantClass, 0, len(b))
 			var i int
@@ -288,9 +348,26 @@ func run() {
 	}
 
 	cancel()
-	<-frameChan
-	<-resultChan
+
+exit_frameChan:
+	for {
+		select {
+		case <-frameChan:
+		default:
+			break exit_frameChan
+		}
+	}
+
+exit_resultChan:
+	for {
+		select {
+		case <-resultChan:
+		default:
+			break exit_resultChan
+		}
+	}
 	wg.Wait()
+
 	close(frameChan)
 	close(resultChan)
 }
@@ -298,8 +375,10 @@ func run() {
 func detect(wg *sync.WaitGroup, resultChan chan<- result, frameChan <-chan image.Image, ctx context.Context, interpreter *tflite.Interpreter, wanted_width, wanted_height, wanted_channels int) {
 	defer wg.Done()
 
-	bb := make([]byte, wanted_width*wanted_height*wanted_channels)
 	input := interpreter.GetInputTensor(0)
+	qp := input.QuantizationParams()
+	bb := make([]byte, wanted_width*wanted_height*wanted_channels)
+	ff := make([]float32, wanted_width*wanted_height*wanted_channels)
 
 	for {
 		select {
@@ -307,15 +386,27 @@ func detect(wg *sync.WaitGroup, resultChan chan<- result, frameChan <-chan image
 			return
 		case img := <-frameChan:
 			resized := resize.Resize(uint(wanted_width), uint(wanted_height), img, resize.NearestNeighbor)
-			for y := 0; y < wanted_height; y++ {
-				for x := 0; x < wanted_width; x++ {
-					r, g, b, _ := resized.At(x, y).RGBA()
-					bb[(y*wanted_width+x)*3+0] = byte(float64(b) / 255.0)
-					bb[(y*wanted_width+x)*3+1] = byte(float64(g) / 255.0)
-					bb[(y*wanted_width+x)*3+2] = byte(float64(r) / 255.0)
+			if input.Type() == tflite.Float32 {
+				for y := 0; y < wanted_height; y++ {
+					for x := 0; x < wanted_width; x++ {
+						r, g, b, _ := resized.At(x, y).RGBA()
+						ff[(y*wanted_width+x)*3+0] = float32(float64(int(b)-qp.ZeroPoint) * qp.Scale)
+						ff[(y*wanted_width+x)*3+1] = float32(float64(int(g)-qp.ZeroPoint) * qp.Scale)
+						ff[(y*wanted_width+x)*3+2] = float32(float64(int(r)-qp.ZeroPoint) * qp.Scale)
+					}
 				}
+				copy(input.Float32s(), ff)
+			} else {
+				for y := 0; y < wanted_height; y++ {
+					for x := 0; x < wanted_width; x++ {
+						r, g, b, _ := resized.At(x, y).RGBA()
+						bb[(y*wanted_width+x)*3+0] = byte(float64(int(b)-qp.ZeroPoint) * qp.Scale)
+						bb[(y*wanted_width+x)*3+1] = byte(float64(int(g)-qp.ZeroPoint) * qp.Scale)
+						bb[(y*wanted_width+x)*3+2] = byte(float64(int(r)-qp.ZeroPoint) * qp.Scale)
+					}
+				}
+				copy(input.UInt8s(), bb)
 			}
-			copy(input.UInt8s(), bb)
 			status := interpreter.Invoke()
 			if status != tflite.OK {
 				log.Fatal("invoke failed")
@@ -323,27 +414,38 @@ func detect(wg *sync.WaitGroup, resultChan chan<- result, frameChan <-chan image
 
 			output := interpreter.GetOutputTensor(0)
 			if output.Type() == tflite.Float32 {
-				var loc [10][4]float32
-				var clazz [10]float32
-				var score [10]float32
-				var nums [1]float32
-				output.CopyToBuffer(&loc[0])
-				interpreter.GetOutputTensor(1).CopyToBuffer(&clazz[0])
-				interpreter.GetOutputTensor(2).CopyToBuffer(&score[0])
-				interpreter.GetOutputTensor(3).CopyToBuffer(&nums[0])
-				num := int(nums[0])
+				if interpreter.GetOutputTensorCount() == 4 {
+					var loc [10][4]float32
+					var clazz [10]float32
+					var score [10]float32
+					var nums [1]float32
+					output.CopyToBuffer(&loc[0])
+					interpreter.GetOutputTensor(1).CopyToBuffer(&clazz[0])
+					interpreter.GetOutputTensor(2).CopyToBuffer(&score[0])
+					interpreter.GetOutputTensor(3).CopyToBuffer(&nums[0])
+					num := int(nums[0])
 
-				resultChan <- &ssdResult{
-					loc:   loc[:num],
-					clazz: clazz[:num],
-					score: score[:num],
-					img:   img,
+					resultChan <- &ssdResult{
+						loc:   loc[:num],
+						clazz: clazz[:num],
+						score: score[:num],
+						img:   img,
+					}
+				} else {
+					output_size := output.Dim(output.NumDims() - 1)
+					f := make([]float32, output_size)
+					copy(f, output.Float32s())
+					resultChan <- &quantFloat32Result{
+						output: f,
+						img:    img,
+					}
 				}
 			} else {
+				output := interpreter.GetOutputTensor(1)
 				output_size := output.Dim(output.NumDims() - 1)
 				b := make([]byte, output_size)
 				copy(b, output.UInt8s())
-				resultChan <- &quantResult{
+				resultChan <- &quantUInt8Result{
 					output: b,
 					img:    img,
 				}

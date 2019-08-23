7ceb020e0bf6551b60d9c4d21b9d17dbb1f8d0fe
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2019-08-23 20:01:06 +0900

    Add OpenCL delegates

---
+++ delegates/gpu/cl/cl.go.h
@@ -0,0 +1,12 @@
+#ifndef GO_TFLITE_H
+#define GO_TFLITE_H
+
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <tensorflow/lite/experimental/c/c_api.h>
+#include <tensorflow/lite/delegates/gpu/cl/gpu_api_delegate.h>
+
+#endif
+

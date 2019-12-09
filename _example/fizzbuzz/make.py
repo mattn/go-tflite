9931a225b3dfa96d92fce8b03cb45a1a3db121c0
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2019-12-09 14:45:52 +0900

    Use TensorFlow 2.0

--- _example/fizzbuzz/make.py
+++ _example/fizzbuzz/make.py
@@ -1,28 +1,35 @@
 import numpy as np
-from tensorflow.contrib.keras.api.keras.models import Sequential, model_from_json
-from tensorflow.contrib.keras.api.keras.layers import Dense, Dropout, Activation
-from tensorflow.contrib.keras.api.keras.optimizers import SGD, Adam
-import tensorflow.contrib.lite as lite
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Activation
+from tensorflow.lite.python import lite
+
 
 def fizzbuzz(i):
-    if   i % 15 == 0: return np.array([0, 0, 0, 1])
-    elif i % 5  == 0: return np.array([0, 0, 1, 0])
-    elif i % 3  == 0: return np.array([0, 1, 0, 0])
-    else:             return np.array([1, 0, 0, 0])
+    if i % 15 == 0:
+        return np.array([0, 0, 0, 1])
+    elif i % 5 == 0:
+        return np.array([0, 0, 1, 0])
+    elif i % 3 == 0:
+        return np.array([0, 1, 0, 0])
+    else:
+        return np.array([1, 0, 0, 0])
+
 
 def bin(i, num_digits):
     return np.array([i >> d & 1 for d in range(num_digits)])
 
-NUM_DIGITS = 7
-trX = np.array([bin(i, NUM_DIGITS) for i in range(1, 101)])
+
+trX = np.array([bin(i, 7) for i in range(1, 101)])
 trY = np.array([fizzbuzz(i) for i in range(1, 101)])
 model = Sequential()
-model.add(Dense(64, input_dim = 7))
+model.add(Dense(64, input_dim=7))
 model.add(Activation('tanh'))
-model.add(Dense(4, input_dim = 64))
+model.add(Dense(4, input_dim=64))
 model.add(Activation('softmax'))
-model.compile(loss = 'categorical_crossentropy', optimizer = 'adam', metrics = ['accuracy'])
-model.fit(trX, trY, epochs = 3600, batch_size = 64)
+model.compile(
+    loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.fit(trX, trY, epochs=3600, batch_size=64)
 model.save('fizzbuzz_model.h5')
 
 converter = lite.TFLiteConverter.from_keras_model_file('fizzbuzz_model.h5')
--- _example/sin/make.py
+++ _example/sin/make.py
@@ -1,19 +1,20 @@
-import numpy as np 
-from tensorflow.contrib.keras.api.keras.models import Sequential, model_from_json
-from tensorflow.contrib.keras.api.keras.layers import Dense, Dropout, Activation
-from tensorflow.contrib.keras.api.keras.optimizers import SGD, Adam
-import tensorflow.contrib.lite as lite
+import numpy as np
 
-data = np.loadtxt('sin.csv', delimiter = ',', unpack = True)
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Activation
+from tensorflow.keras.optimizers import Adam
+from tensorflow.lite.python import lite
+
+data = np.loadtxt('sin.csv', delimiter=',', unpack=True)
 model = Sequential()
-model.add(Dense(30, input_shape = (1, )))
+model.add(Dense(30, input_shape=(1, )))
 model.add(Activation('sigmoid'))
 model.add(Dense(40))
 model.add(Activation('sigmoid'))
 model.add(Dense(1))
-sgd = Adam(lr = 0.1)
-model.compile(loss = 'mean_squared_error', optimizer = sgd)
-model.fit(data[0], data[1], epochs = 1000, batch_size = 20, verbose = 0)
+sgd = Adam(lr=0.1)
+model.compile(loss='mean_squared_error', optimizer=sgd)
+model.fit(data[0], data[1], epochs=1000, batch_size=20, verbose=0)
 model.save('sin_model.h5')
 
 converter = lite.TFLiteConverter.from_keras_model_file('sin_model.h5')

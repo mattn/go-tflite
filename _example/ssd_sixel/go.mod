5397bb68c1ccb05228c07a87cd0badc5e110f558
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2026-06-20 18:47:47 +0900

    Add ssd_sixel

---
+++ _example/ssd_sixel/go.mod
@@ -0,0 +1,17 @@
+module github.com/mattn/go-tflite/_example/ssd
+
+go 1.25.0
+
+replace github.com/mattn/go-tflite => ../..
+
+require (
+	github.com/mattn/go-sixel v0.0.11
+	github.com/mattn/go-tflite v1.0.5
+	gocv.io/x/gocv v0.43.0
+	golang.org/x/image v0.43.0
+)
+
+require (
+	github.com/mattn/go-pointer v0.0.1 // indirect
+	github.com/soniakeys/quant v1.0.0 // indirect
+)
---
+++ _example/ssd_sixel/main.go
@@ -0,0 +1,263 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"flag"
+	"fmt"
+	"image"
+	"log"
+	"os"
+	"os/signal"
+	"sort"
+	"sync"
+	"time"
+
+	"github.com/mattn/go-sixel"
+	"github.com/mattn/go-tflite"
+
+	"gocv.io/x/gocv"
+	"golang.org/x/image/colornames"
+)
+
+var (
+	video     = flag.String("camera", "0", "video cature")
+	modelPath = flag.String("model", "detect.tflite", "path to model file")
+	labelPath = flag.String("label", "labelmap.txt", "path to label file")
+	limits    = flag.Int("limits", 5, "limits of items")
+)
+
+type ssdResult struct {
+	loc   []float32
+	clazz []float32
+	score []float32
+	mat   gocv.Mat
+}
+
+type ssdClass struct {
+	loc   []float32
+	score float64
+	index int
+}
+
+func loadLabels(filename string) ([]string, error) {
+	labels := []string{}
+	f, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		labels = append(labels, scanner.Text())
+	}
+	return labels, nil
+}
+
+func copySlice(f []float32) []float32 {
+	ff := make([]float32, len(f), len(f))
+	copy(ff, f)
+	return ff
+}
+
+func detect(ctx context.Context, wg *sync.WaitGroup, resultChan chan<- *ssdResult, interpreter *tflite.Interpreter, wanted_width, wanted_height, wanted_channels int, cam *gocv.VideoCapture) {
+	defer wg.Done()
+	defer close(resultChan)
+
+	input := interpreter.GetInputTensor(0)
+	qp := input.QuantizationParams()
+	log.Printf("width: %v, height: %v, type: %v, scale: %v, zeropoint: %v", wanted_width, wanted_height, input.Type(), qp.Scale, qp.ZeroPoint)
+	log.Printf("input tensor count: %v, output tensor count: %v", interpreter.GetInputTensorCount(), interpreter.GetOutputTensorCount())
+	if qp.Scale == 0 {
+		qp.Scale = 1
+	}
+
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		default:
+		}
+
+		if len(resultChan) == cap(resultChan) {
+			continue
+		}
+
+		frame := gocv.NewMat()
+		if ok := cam.Read(&frame); !ok {
+			frame.Close()
+			break
+		}
+
+		resized := gocv.NewMat()
+		if input.Type() == tflite.Float32 {
+			frame.ConvertTo(&resized, gocv.MatTypeCV32F)
+			gocv.Resize(resized, &resized, image.Pt(wanted_width, wanted_height), 0, 0, gocv.InterpolationDefault)
+			if ff, err := resized.DataPtrFloat32(); err == nil {
+				copy(input.Float32s(), ff)
+			}
+		} else {
+			gocv.Resize(frame, &resized, image.Pt(wanted_width, wanted_height), 0, 0, gocv.InterpolationDefault)
+			if v, err := resized.DataPtrUint8(); err == nil {
+				copy(input.UInt8s(), v)
+			}
+		}
+		resized.Close()
+		status := interpreter.Invoke()
+		if status != tflite.OK {
+			log.Println("invoke failed")
+			return
+		}
+
+		resultChan <- &ssdResult{
+			loc:   copySlice(interpreter.GetOutputTensor(0).Float32s()),
+			clazz: copySlice(interpreter.GetOutputTensor(1).Float32s()),
+			score: copySlice(interpreter.GetOutputTensor(2).Float32s()),
+			mat:   frame,
+		}
+	}
+}
+
+func main() {
+	flag.Parse()
+
+	labels, err := loadLabels(*labelPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+
+	cam, err := gocv.OpenVideoCapture(*video)
+	if err != nil {
+		log.Printf("cannot open camera: %v", err)
+		return
+	}
+	defer cam.Close()
+
+	model := tflite.NewModelFromFile(*modelPath)
+	if model == nil {
+		log.Println("cannot load model")
+		return
+	}
+	defer model.Delete()
+
+	options := tflite.NewInterpreterOptions()
+	options.SetNumThread(4)
+	//options.AddDelegate(cl.New(nil))
+	defer options.Delete()
+
+	interpreter := tflite.NewInterpreter(model, options)
+	if interpreter == nil {
+		log.Println("cannot create interpreter")
+		return
+	}
+	defer interpreter.Delete()
+
+	status := interpreter.AllocateTensors()
+	if status != tflite.OK {
+		log.Println("allocate failed")
+		return
+	}
+
+	input := interpreter.GetInputTensor(0)
+	wanted_height := input.Dim(1)
+	wanted_width := input.Dim(2)
+	wanted_channels := input.Dim(3)
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	// Start up the background capture
+	resultChan := make(chan *ssdResult, 1)
+	go detect(ctx, &wg, resultChan, interpreter, wanted_width, wanted_height, wanted_channels, cam)
+
+	sc := make(chan os.Signal, 1)
+	defer close(sc)
+	signal.Notify(sc, os.Interrupt)
+	go func() {
+		<-sc
+		cancel()
+	}()
+
+	// Some local vars to calculate frame rate
+	var (
+		frames = 0
+		second = time.Tick(time.Second)
+	)
+
+	enc := sixel.NewEncoder(os.Stdout)
+
+	// Clear the screen and hide the cursor while drawing.
+	fmt.Print("\x1b[2J\x1b[?25l")
+	defer fmt.Print("\x1b[?25h")
+
+	for {
+		// Run inference if we have a new frame to read
+		result, ok := <-resultChan
+		if !ok {
+			break
+		}
+
+		classes := make([]ssdClass, 0, len(result.clazz))
+		var i int
+		for i = 0; i < len(result.clazz); i++ {
+			idx := int(result.clazz[i] + 1)
+			score := float64(result.score[i])
+			if score < 0.6 {
+				continue
+			}
+			classes = append(classes, ssdClass{loc: result.loc[i*4 : (i+1)*4], score: score, index: idx})
+		}
+		sort.Slice(classes, func(i, j int) bool {
+			return classes[i].score > classes[j].score
+		})
+		if len(classes) > *limits {
+			classes = classes[:*limits]
+		}
+
+		size := result.mat.Size()
+		for i, class := range classes {
+			label := "unknown"
+			if class.index < len(labels) {
+				label = labels[class.index]
+			}
+			c := colornames.Map[colornames.Names[class.index%len(colornames.Names)]]
+			gocv.Rectangle(&result.mat, image.Rect(
+				int(float32(size[1])*class.loc[1]),
+				int(float32(size[0])*class.loc[0]),
+				int(float32(size[1])*class.loc[3]),
+				int(float32(size[0])*class.loc[2]),
+			), c, 2)
+			text := fmt.Sprintf("%d %.5f %s", i, class.score, label)
+			gocv.PutText(&result.mat, text, image.Pt(
+				int(float32(size[1])*class.loc[1]),
+				int(float32(size[0])*class.loc[0]),
+			), gocv.FontHersheySimplex, 1.2, c, 1)
+		}
+
+		img, err := result.mat.ToImage()
+		result.mat.Close()
+		if err != nil {
+			log.Println(err)
+			break
+		}
+
+		// Move the cursor to the top-left and draw the frame as sixel.
+		fmt.Print("\x1b[H")
+		enc.Encode(img)
+
+		// calculate frame rate
+		frames++
+		select {
+		case <-second:
+			// Update the terminal title with the current FPS.
+			fmt.Printf("\x1b]0;SSD | FPS: %d\x07", frames)
+			frames = 0
+		default:
+		}
+	}
+
+	cancel()
+	wg.Wait()
+}

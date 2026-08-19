4cbc4099c269fc6044ba41ff4528cc5ab5e153aa
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2026-08-19 09:50:32 +0900

    Expose XNNPACK delegate flags

--- delegates/xnnpack/xnnpack.go
+++ delegates/xnnpack/xnnpack.go
@@ -13,8 +13,36 @@ import (
 	"github.com/mattn/go-tflite/delegates"
 )
 
+// Flags to enable optional features of the delegate. See the Flag constants.
+type Flags uint32
+
+const (
+	// FlagQS8 enable XNNPACK acceleration for signed quantized 8-bit inference.
+	FlagQS8 Flags = C.TFLITE_XNNPACK_DELEGATE_FLAG_QS8
+	// FlagQU8 enable XNNPACK acceleration for unsigned quantized 8-bit inference.
+	FlagQU8 Flags = C.TFLITE_XNNPACK_DELEGATE_FLAG_QU8
+	// FlagForceFP16 force FP16 inference for FP32 operators.
+	FlagForceFP16 Flags = C.TFLITE_XNNPACK_DELEGATE_FLAG_FORCE_FP16
+	// FlagDynamicFullyConnected enable XNNPACK acceleration for FULLY_CONNECTED
+	// operator with dynamic weights.
+	FlagDynamicFullyConnected Flags = C.TFLITE_XNNPACK_DELEGATE_FLAG_DYNAMIC_FULLY_CONNECTED
+	// FlagVariableOperators enable XNNPACK acceleration for VAR_HANDLE,
+	// READ_VARIABLE, and ASSIGN_VARIABLE operators.
+	FlagVariableOperators Flags = C.TFLITE_XNNPACK_DELEGATE_FLAG_VARIABLE_OPERATORS
+	// FlagTransientIndirectionBuffer enable transient indirection buffer to
+	// reduce memory usage in selected operators.
+	FlagTransientIndirectionBuffer Flags = C.TFLITE_XNNPACK_DELEGATE_FLAG_TRANSIENT_INDIRECTION_BUFFER
+	// FlagEnableLatestOperators enable the latest XNNPACK operators and features.
+	FlagEnableLatestOperators Flags = C.TFLITE_XNNPACK_DELEGATE_FLAG_ENABLE_LATEST_OPERATORS
+	// FlagEnableSubgraphReshaping enable XNNPack subgraph reshaping.
+	FlagEnableSubgraphReshaping Flags = C.TFLITE_XNNPACK_DELEGATE_FLAG_ENABLE_SUBGRAPH_RESHAPING
+)
+
 type DelegateOptions struct {
 	NumThreads int32
+
+	// Flags override the default feature flags of the delegate when non-zero.
+	Flags Flags
 }
 
 // Delegate is the tflite delegate
@@ -26,6 +54,9 @@ func New(options DelegateOptions) delegates.Delegater {
 	var d *C.TfLiteDelegate
 	coptions := C.TfLiteXNNPackDelegateOptionsDefault()
 	coptions.num_threads = C.int32_t(options.NumThreads)
+	if options.Flags != 0 {
+		coptions.flags = C.uint32_t(options.Flags)
+	}
 	d = C.TfLiteXNNPackDelegateCreate(&coptions)
 	if d == nil {
 		return nil
@@ -43,4 +74,9 @@ func (d *Delegate) Delete() {
 // Return a pointer
 func (d *Delegate) Ptr() unsafe.Pointer {
 	return unsafe.Pointer(d.d)
+}
+
+// Flags return the feature flags the delegate is using.
+func (d *Delegate) Flags() Flags {
+	return Flags(C.TfLiteXNNPackDelegateGetFlags(d.d))
 }
